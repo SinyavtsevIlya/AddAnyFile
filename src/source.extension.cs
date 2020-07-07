03cbaf2318f696101b5147b2d3d2447ec1ff5880
Sinyavtsev Ilya <SinyavtsevIlya@gmail.com>
2020-07-07 19:25:28 +0300

    Manifest updated

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -12,7 +12,7 @@ namespace MadsKristensen.AddAnyFile
 								public const string Description = @"The fastest and easiest way to add new files to any project - including files that start with a dot";
 								public const string Language = "en-US";
 								public const string Version = "3.5";
-								public const string Author = "Mads Kristensen";
+								public const string Author = "Ilya Sinyavtsev";
 								public const string Tags = "file, add, template";
 				}
 }
